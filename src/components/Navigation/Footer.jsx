f8b97221fbe5fa6742bbb75aa01604b9a1fe8afb
Moejr <mo3jrr@gmail.com>
2024-01-03 19:19:24 +0300

    updated to make it detects more than one face, I also updated a few other changes

--- src/components/Navigation/Footer.jsx
+++ src/components/Navigation/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div>
       <footer className="ph3 ph4-ns pv6 bt ">
-         <p className="f7 db lh-solid white">© 2023 BrainScape. built and designed by <a className="link dim white pointer" href="https://twitter.com/m0ejr" target="_blank">@Moejr</a></p>
+         <p className="f7 db lh-solid white">© 2024 BrainScape. built and designed by <a className="link dim white pointer" href="https://moejr.dev" target="_blank">Moejr</a></p>
       </footer>
     </div>
   );
